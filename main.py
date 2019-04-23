c8bcf2876667adbbc97b2a42a8ea37af84a51f40
Alexander Wurts <ajwurts@gmail.com>
2019-04-23 15:08:07 -0400

    Flipflop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import scipy.optimize
+from sklearn.decomposition import PCA
 
 NUM_INPUT = 784  # Number of input neurons
 NUM_HIDDEN = 40  # Number of hidden neurons
@@ -41,7 +42,10 @@ def unpack(w):
 def pack(W1, b1, W2, b2):
     w1_flattened = W1.reshape((W1.shape[0] * W1.shape[1],))
     w2_flattened = W2.reshape((W2.shape[0] * W2.shape[1],))
-    result = np.concatenate((w1_flattened, b1, w2_flattened, b2))
+    b1_flattened = b1.reshape((b1.shape[0],))
+    b2_flattened = b2.reshape((b2.shape[0],))
+    result = np.concatenate(
+        (w1_flattened, b1_flattened, w2_flattened, b2_flattened))
     return result
 
 # Load the images and labels from a specified dataset (train or test).
@@ -60,10 +64,17 @@ def plotSGDPath(trainX, trainY, ws):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
+    # Use This function
+    # Also use inverse_transform too
+
+    clf = PCA(n_components=2)
+    clf.fit(ws)
+
     # Compute the CE loss on a grid of points (corresonding to different w).
     axis1 = np.arange(-np.pi, +np.pi, 0.05)  # Just an example
     axis2 = np.arange(-np.pi, +np.pi, 0.05)  # Just an example
     Xaxis, Yaxis = np.meshgrid(axis1, axis2)
+    # Base axis on weights for 15000
     Zaxis = np.zeros((len(axis1), len(axis2)))
     for i in range(len(axis1)):
         for j in range(len(axis2)):
@@ -95,9 +106,9 @@ def predict(X, w):
 
     z1 = (W1.T.dot(X.T).T + b1).T
     h1 = reluPrime(z1)
-    z2 = (W2.T.dot(h1).T + b2).T
-    yhat = softmax(z2)
-    # yHat is many results in one there X.shape[0] by 10
+    z2 = W2.T.dot(h1).T + b2
+    yhat = softmax(z2.T)
+
     return yhat
 
 # Given training images X, associated labels Y, and a vector of combined weights
@@ -127,7 +138,7 @@ def gradCE(X, Y, w):
     z1 = (W1.T.dot(X.T).T + b1)
     h1 = reluPrime(z1)
     z2 = (W2.T.dot(h1.T).T + b2)
-    yhat = softmax(z2)
+    yhat = softmax(z2.T)
 
     yHatMinusY = yhat - Y.T
 
@@ -138,25 +149,48 @@ def gradCE(X, Y, w):
     grad_w1 = g @ X
     grad_b1 = np.mean(g, axis=1)
 
-    return pack(grad_w1.T, grad_b1, grad_w2.T, grad_b2)
+    return pack(grad_w1, grad_b1, grad_w2, grad_b2)
 
 
 def oneForwardProp(X, Y, w):
     W1, b1, W2, b2 = unpack(w)
 
-    z1 = W1.dot(X) + b1
+    z1 = W1.T.dot(X.T).T + b1
     h1 = reluPrime(z1)
-    z2 = W2.dot(h1) + b2
+    z2 = W2.T.dot(h1.T).T + b2
+    yhat = softmax(z2.T)
+
+    yHatMinusY = (yhat - Y).T
+
+    g = ((yHatMinusY.T @ W2.T).T * reluPrime(z1.T)).T
+
+    grad_w2 = yHatMinusY @ h1
+    grad_b2 = np.mean(yHatMinusY, axis=1)
+    grad_w1 = g.T @ X
+    grad_b1 = np.mean(g, axis=0)
+
+    return pack(grad_w1.T, grad_b1, grad_w2.T, grad_b2)
+
+
+def tryThree(X, y, w):
+    W1, b1, W2, b2 = unpack(w)
+
+    z1 = W1 @ X + b1
+    h1 = reluPrime(z1)
+    z2 = W2 @ h1 + b2
     yhat = softmax(z2)
 
-    yHatMinusY = yhat - Y
+    yhat_y = yhat - y
 
-    g = ((yHatMinusY @ W2.T) * reluPrime(z1.T)).T
+    gT = (yhat - y).T * reluPrime(z1.T)
+    g = gT.T
 
-    grad_w2 = yHatMinusY.T @ h1.T
-    grad_b2 = yHatMinusY
+    grad_w2 = yhat_y @ h1.T
+    grad_b2 = yhat_y
     grad_w1 = g @ X.T
     grad_b1 = g
+
+    return grad_w2, grad_b2, grad_w1, grad_b1
 
 
 def backprop(X, Y, w):
@@ -176,7 +210,7 @@ def backprop(X, Y, w):
     grad_w1 = g @ X.T
     grad_b1 = np.mean(g, axis=1)
 
-    return pack(w)  # [grad_w1, grad_b1, grad_w2, grad_b2]
+    return grad_w1, grad_b1, grad_w2, grad_b2
 
 # Given training and testing datasets and an initial set of weights/biases b,
 # train the NN. Then return the sequence of w's obtained during SGD.
@@ -230,29 +264,42 @@ if __name__ == "__main__":
 
     print("Loaded Data")
     # # Initialize weights randomly
-    W1 = 2*(np.random.random(size=(NUM_INPUT, NUM_HIDDEN)) /
+    # W1 = 2*(np.random.random(size=(NUM_INPUT, NUM_HIDDEN)) /
+    #         NUM_INPUT**0.5) - 1./NUM_INPUT**0.5
+    # b1 = 0.01 * np.ones(NUM_HIDDEN)
+    # W2 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_OUTPUT)) /
+    #         NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5
+    # b2 = 0.01 * np.ones(NUM_OUTPUT)
+    W1 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_INPUT)) /
             NUM_INPUT**0.5) - 1./NUM_INPUT**0.5
     b1 = 0.01 * np.ones(NUM_HIDDEN)
-    W2 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_OUTPUT)) /
+    W2 = 2*(np.random.random(size=(NUM_OUTPUT, NUM_HIDDEN)) /
             NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5
     b2 = 0.01 * np.ones(NUM_OUTPUT)
     w = pack(W1, b1, W2, b2)
+
     W1t, b1t, W2t, b2t = unpack(pack(W1, b1, W2, b2))
+    assert np.all(W1 == W1t)
+    assert np.all(b1 == b1t)
+    assert np.all(W2 == W2t)
+    assert np.all(b2 == b2t)
 
     # Check that the gradient is correct on just a few examples (randomly drawn)## Use check grad on each individualW1, W2, b1, b2
+    # idxs = np.random.permutation(trainX.shape[0])[0:NUM_CHECK]
 
-    idxs = np.random.permutation(trainX.shape[0])[0:NUM_CHECK]
-    print(scipy.optimize.check_grad(lambda w_: fCE(np.atleast_2d(trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w_),
-                                    lambda w_: gradCE(np.atleast_2d(
-                                        trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w_),
-                                    w))
-    W1g, b1t, W2t, b2t = unpack(gradCE(np.atleast_2d(trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w))
-    W1a, b1a, W2a, b2a = unpack(scipy.optimize.approx_fprime(w, lambda w_: fCE(np.atleast_2d(
-        trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w_), 1.49e-08))
-    print("W1: ", np.sum(W1t - W1a))
-    print("b1: ", np.sum(b1t - b1a))
-    print("W2: ", np.sum(W2t - W2a))
-    print("b2: ", np.sum(b2t - b2a))
+    tryThree(trainX[0:1,:], trainY[0:1, :], w)
+    # print(scipy.optimize.check_grad(lambda w_: fCE(np.atleast_2d(trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w_),
+    #                                 lambda w_: oneForwardProp(np.atleast_2d(
+    #                                     trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w_),
+    #                                 w))
+    # W1g, b1t, W2t, b2t = unpack(oneForwardProp(np.atleast_2d(
+    #     trainX[0:1, :]), np.atleast_2d(trainY[0:1, :]), w))
+    # W1a, b1a, W2a, b2a = unpack(scipy.optimize.approx_fprime(w, lambda w_: fCE(np.atleast_2d(
+    #     trainX[0:1, :]), np.atleast_2d(trainY[0:1, :]), w_), 1.49e-08))
+    # print("W1: ", np.sqrt(np.sum((W1t - W1a) ** 2)))
+    # print("b1: ", np.sqrt(np.sum((b1t - b1a) ** 2)))
+    # print("W2: ", np.sqrt(np.sum((W2t - W2a) ** 2)))
+    # print("b2: ", np.sqrt(np.sum((b2t - b2a) ** 2)))
 
     # # # Train the network and obtain the sequence of w's obtained using SGD.
     # ws = train(trainX.T, trainY, optX.T, optY, w)

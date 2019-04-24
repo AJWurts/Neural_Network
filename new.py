6fac5cb5c389007c4d8db7f3544c3d20e43e8ff0
Alexander Wurts <ajwurts@gmail.com>
2019-04-23 22:31:19 -0400

    Added final document and changed python filename

--- new.py
+++
@@ -1,9 +0,0 @@
-from scipy.optimize import check_grad
-
-def func(x):
-  return x[0]**2 - 0.5 * x[1]**3
-def grad(x):
-  return [2 * x[0], -1.5 * x[1]**2]
-
-
-print(check_grad(func, grad, [1.5, -1.5]))
